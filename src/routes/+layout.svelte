b1581e068cb023f14e10f4aa21e51caeb5000e9d
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2024-02-28 19:23:46 +0100

    kurwa

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<nav>
+    <marquee>
+    <a class="btn" href="/">Home</a>
+    <a class="btn" href="/onas">onas</a>
+    <a class="btn" href="/">Home</a>
+    </marquee>
+</nav>
+
+<style>
+    nav {
+        background-color: #808080;
+    }
+
+    .btn {
+        border: 6px ridge #c0c0c0;
+        animation: blink 1s infinite;
+        text-align: center;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    @keyframes
+    blink {
+        0% {
+        background-color: #c0c0c0;
+        }
+        50% {
+        background-color: #ffffffec;
+        }
+        100% {
+        background-color: #c0c0c0;
+        }
+    }
+</style>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
+<script>
+    import logo from '$lib/images/logo.gif'; 
+    import hacker from '$lib/images/hacker3d.gif';
+    import earth from '$lib/images/earth.gif';
+</script>
+
+
+<div class="welcome-div">
+    <img src={earth} alt="asfdas"/>
+<img src={hacker} alt="asfdas"/> <img src={logo} alt="asfdas"/>
+<img src={hacker} alt="asfdas"/>
+</div>
+
+<div class="main content box">
+
+</div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+    .welcome-div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    h1 {
+        text-align: center;
+    }
+    p {
+        text-align: center;
+    }
+</style>
